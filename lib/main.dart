a1b34d7b83bb6339f088d0f37d0bf7d005c34ce2
theodorous.yuvens <theodorus_beatayuvens@aeon.co.id>
2021-03-16 11:35:03 +0700

    add circle avatar

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
-  );
+  runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/penguins.jpg'),
+              ),
+              Text(
+                'Penguins',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
